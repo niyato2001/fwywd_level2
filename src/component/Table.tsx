6f36505c4a98080ec72e0a415dc1f7593a914c4e
niyato2001 <niyato2001@gmail.com>
2022-07-24 15:53:49 +0900

    feat: localStorageの実装

--- src/component/Table.tsx
+++ src/component/Table.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface ToDoProps {
   isCompleted: boolean;
@@ -7,13 +7,15 @@ interface ToDoProps {
 }
 
 export const Table: React.FC = () => {
-  const initialtoDos: ToDoProps[] = [
-    {
-      isCompleted: false,
-      name: 'task1',
-      description: 'ねる　',
-    },
-  ];
+  useEffect(() => {
+    setToDos(fetchToDos());
+  }, []);
+
+  const fetchToDos = (): ToDoProps[] => {
+    const data = localStorage.getItem('todo');
+    if (data) return JSON.parse(data);
+    return [];
+  };
 
   // const [nameForm, setNameForm] = useState<string>('');
   // const [descriptionForm, setDescriptionForm] = useState<string>('');
@@ -24,13 +26,14 @@ export const Table: React.FC = () => {
   };
   const initialEditedNumber = -1;
   const [formState, setFormState] = useState<ToDoProps>(initialFormState);
-  const [toDos, setToDos] = useState<ToDoProps[]>(initialtoDos);
+  const [toDos, setToDos] = useState<ToDoProps[]>([]);
   const [isEdited, setIsEdited] = useState<boolean>(false);
   const [editedNumber, setEditedNumber] = useState<number>(initialEditedNumber);
   const newClick = () => {
     if (!formState.name || !formState.description) return;
-    setToDos([...toDos, { ...formState, isCompleted: false }]);
-
+    const newToDos: ToDoProps[] = [...toDos, { ...formState, isCompleted: false }];
+    setToDos(newToDos);
+    localStorage.setItem('todo', JSON.stringify(newToDos));
     setFormState(initialFormState);
   };
   const editedClick = (toDo: ToDoProps, i: number) => {
@@ -42,8 +45,8 @@ export const Table: React.FC = () => {
     if (!confirm('本当に削除しますか？')) return;
     const newToDos: ToDoProps[] = [...toDos];
     newToDos.splice(i, 1);
-
     setToDos(newToDos);
+    localStorage.setItem('todo', JSON.stringify(newToDos));
   };
   const updateClick = () => {
     if (!formState.name || !formState.description) return;
@@ -52,8 +55,7 @@ export const Table: React.FC = () => {
     newToDos[editedNumber] = formState;
     setToDos(newToDos);
     // setToDos(toDos.splice(editedNumber, 0, formState));
-    const json: string = JSON.stringify(newToDos, undefined, 1);
-    localStorage.setItem('key', json);
+    localStorage.setItem('todo', JSON.stringify(newToDos));
     setFormState(initialFormState);
     setEditedNumber(initialEditedNumber);
     setIsEdited(false);
@@ -71,6 +73,7 @@ export const Table: React.FC = () => {
     const updatedToDos: ToDoProps[] = [...toDos];
     updatedToDos[i] = { ...updatedToDos[i], isCompleted: checked };
     setToDos(updatedToDos);
+    localStorage.setItem('todo', JSON.stringify(updatedToDos));
   };
   return (
     <div className='my-20'>
@@ -92,6 +95,7 @@ export const Table: React.FC = () => {
                   checked={toDo.isCompleted}
                   onChange={(e) => handleCheck(e.target.checked, i)}
                 />
+                {/*HTMLでは 属性としてcheckedしかないがReactでは状態を反映するcheckedと反映しないdefaultCheckedが存在する。 */}
               </td>
               <td className='px-4 py-2'>{toDo.name}</td>
               <td className='px-4 py-2'>{toDo.description}</td>
